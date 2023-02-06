8e63e34b6489737db7d9fdaa739f2928015a6f70
Ricardo Beaujon <rbeaujon77@gmail.com>
2023-02-06 22:34:55 +0200

    refactor: removed some files

--- src/helper/API/registerData.api.js
+++
@@ -1,8 +0,0 @@
-const registerData = async (header) => {
-
-  const url = "http://localhost:3001/register";
-  const response = await fetch(url, header);
-  return response.status;
-}
-
-export default registerData
