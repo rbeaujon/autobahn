12693e5c93c25262ec18ac8cbb5a3885a63ba695
Ricardo Beaujon <rbeaujon77@gmail.com>
2023-02-07 11:13:30 +0200

    refactor(BE Verification): session id verification was modified to manage string (uuid), removed console.log()

--- BE/helper/verification.js
+++ BE/helper/verification.js
@@ -2,12 +2,11 @@ const Verification = (userId, fieldId, session) => {
 
   // Return 3  values Empty, Type, Ok
 
-console.log(`Verification data received: A ${userId}, B ${fieldId}, C ${session}` ) 
   if(userId === undefined || fieldId === undefined || session === undefined){
     return "Empty"
   }else if(userId === "" || fieldId === "" || session === ""){
     return "Empty"
-  }else if(typeof userId !== 'string', typeof fieldId !== 'object', typeof session !== 'number'){
+  }else if(typeof userId !== 'string', typeof fieldId !== 'object', typeof session !== 'string'){
     return "Type"
   }else if(userId.length > 0 || fieldId.length > 0 || session.length > 0){
     return "Ok"
